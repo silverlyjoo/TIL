f80e51849504c71dd412d2d7bb686184aec78fc1
silverlyjoo <silverlyjoo@gmail.com>
2019-03-26 21:19:32 +0900

    D59 | 190326 PM | algorithm_perm_ad at home

--- algorithm/190326_perm&powersum/max_price5_final.py
+++ algorithm/190326_perm&powersum/max_price5_final.py
@@ -6,26 +6,23 @@ def gonum(L):
     return int(''.join(map(str, L[:])))
 
 def changeprice(n, cnt, N, data):
-
     if cnt == N:
         result.append(gonum(data))
         return
-
     for i in range(n):
         for j in range(i+1, n):
             data[i], data[j] = data[j], data[i]
-            changeprice(n, cnt+1, N, data)
+            if data not in chk[cnt]:
+                chk[cnt].append(data[:])
+                changeprice(n, cnt+1, N, data)
             data[i], data[j] = data[j], data[i]
-
+        
 T = int(input())
-
 for tc in range(1, T+1):
     nums, N = input().split()
     N = int(N)
     data = list(map(int, nums))
     chk = [[] for _ in range(N)]
-
     result = []
     changeprice(len(data), 0, N, data)
-    R = list(map(int, result))
-    print(max(R))
+    print(max(result))
